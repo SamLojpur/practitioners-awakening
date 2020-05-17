77d19f37a4947452355386840ea19f86c70a24e2
Samuel Lojpur <samlojpur@gmail.com>
2020-05-16 18:27:39 -0600

    core movement working

--- scripts/app.js
+++ scripts/app.js
@@ -25,50 +25,59 @@ SVG.extend(SVG.G, {
 var bg_color = '#483D8B'
 
 
-var draw = SVG().addTo('body').size(2000, 2000).attr({ style: 'background-color:' + bg_color })
-var circle_group = draw.group()
-var core_circle = circle_group.ellipse(200, 200).attr({
+PIVOT_X = 430
+PIVOT_Y = 430
+
+var draw = SVG().addTo('body').size(860, 860).attr({ style: 'background-color:' + bg_color })
+var core_circle = draw.ellipse(100, 100).attr({
     fill: 'none',
     stroke: SVG.Color.random('vibrant'),
     'stroke-width': 6
-}).move(300, 300);
-var circle2_group = circle_group.group()
+}).translate(PIVOT_X-50, PIVOT_Y-50);
 
-circle_group.animate(9000, 0, 'now').ease('-').loop(0).rotate(-360, 400, 400)
+var circle_group = draw.group()
+triangle_ring = makeRing(circle_group, PIVOT_X, PIVOT_Y, 5, 300, makeCircledShape(3, 50, 1.30, -90))
+circle_group.animate(9000, 0, 'now').ease('-').loop(0).rotate(-360, PIVOT_X, PIVOT_Y)
+triangle_ring.forEach( triangle => triangle.animate(9000, 0, 'now').ease('-').loop(0).rotate(360, 0, 0) )
 
-triangle_ring = makeRing(circle_group, 400, 400, 5, 200, makeCircledShape(3, 60, 1.30))
-// console.log(triangle_ring)
-// triangle_ring.forEach( triangle => triangle.animate(3000, 0, 'now').ease('-').loop(0).rotate(-360, 0, 0) )
+var circle_group2 = draw.group()
+triangle_ring = makeRing(circle_group2, PIVOT_X, PIVOT_Y, 7, 500, makeCircledShape(4, 50, 1.30, 45))
+circle_group2.animate(9000, 0, 'now').ease('-').loop(0).rotate(360, PIVOT_X, PIVOT_Y)
+triangle_ring.forEach( triangle => triangle.animate(9000, 0, 'now').ease('-').loop(0).rotate(-360, 0, 0) )
 
-makeCircledShape(3, 100)
+var circle_group3 = draw.group()
+triangle_ring = makeRing(circle_group3, PIVOT_X, PIVOT_Y, 5, 750, makeCircledShape(5, 50, 1.30, -90))
+circle_group3.animate(9000, 0, 'now').ease('-').loop(0).rotate(-360, PIVOT_X, PIVOT_Y)
+triangle_ring.forEach( triangle => triangle.animate(9000, 0, 'now').ease('-').loop(0).rotate(360, 0, 0) )
 
-function makeCircledShape(n, r, scale) {
+
+function makeCircledShape(n, r, scale, rotation) {
     circled_shape_group = draw.group()
     shape_holder_color = SVG.Color.random('vibrant')
     ring_color = SVG.Color.random('vibrant')
 
     polygon_plot = regularPolygonPoints(n, r)
-    shape = circled_shape_group.polygon().plot(polygon_plot).fill('none').stroke({ color: shape_holder_color, width: 6, linejoin: 'round' });
+    shape = circled_shape_group.polygon().plot(polygon_plot).rotate(rotation,0,0).fill('none').stroke({ color: shape_holder_color, width: 6, linejoin: 'round' });
     shape_holder = circled_shape_group.circle(r * scale).translate(-(r * scale) / 2, -(r * scale) / 2).fill('none').stroke({ color: ring_color, width: 6, linejoin: 'round' });
+    circled_shape_group.translate(200,200)
     return circled_shape_group
 }
 
 
 function makeRing(ring_group, cx, cy, n, r, corner_decorator_group) {
-    // mask_test = ring_group.rect(2000,2000).fill('#000').opacity(0.5)
-    //polygon
     plot = regularPolygonPoints(n, r)
+    angles = regularPolygonAngles(n)
 
     //outer ring
-    // ring_color = SVG.Color.random('vibrant')
     ring = ring_group.circle(r)
 
     ring.translate(cx - r / 2, cy - r / 2).fill('none').stroke({ color: ring_color, width: 6, linejoin: 'round' })
 
     //polygon ring
     poly_ring = ring_group.polygon().plot(plot)
-    poly_ring.translate(cy, cx).fill('none').stroke({ color: ring_color, width: 6, linejoin: 'round' })
+    poly_ring.translate(cx, cy).fill('none').stroke({ color: ring_color, width: 6, linejoin: 'round' })
 
+    // circled_shapes = plot.map(([x, y]) => { return corner_decorator_group.deepClone().translate(cx + x, cy + y).rotate(angles.shift() ) })
     circled_shapes = plot.map(([x, y]) => { return corner_decorator_group.deepClone().translate(cx + x, cy + y) })
     circled_shapes.forEach((shape) => {ring_group.add(shape)})
 
@@ -86,12 +95,10 @@ function makeRing(ring_group, cx, cy, n, r, corner_decorator_group) {
     console.log(mask_group.children())
     
     mask_group.front()
-    core_circle.maskWith(a = mask_group.clone())
-    ring.maskWith(b = mask_group.clone())
-    b.animate(3000).translate(- cx + r / 2, - cy + r / 2)
-    poly_ring.maskWith(c = mask_group.clone())
-    c.animate(3000).translate(-cx, -cy)
+    ring.maskWith(mask_group.clone().translate(- cx + r / 2, - cy + r / 2))
+    poly_ring.maskWith(mask_group.clone().translate(-cx, -cy))
     mask_group.remove()
+    corner_decorator_group.remove()
     
     return circled_shapes
 }
@@ -103,10 +110,16 @@ function regularPolygonPoints(n, r) {
         var theta = 2 * Math.PI * i / n
         plot.push([Math.cos(theta) * r / 2, Math.sin(theta) * r / 2])
     }
-    // console.log(plot)
     return plot
 }
-
+function regularPolygonAngles(n) {
+    var angles = []
+    for (i = 0; i < n; i++) {
+        var theta = 360 * i / n
+        angles.push(theta)
+    }
+    return angles
+}
 
 // //shape holder
 // shape_holder_color = SVG.Color.random('vibrant')
